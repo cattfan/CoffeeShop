e4f7a867d0613b8f25369ae2e376292576284b1b
cattfan <cattfan239@gmail.com>
2025-05-27 08:28:33 +0700

    Section4 100%

--- obj/Debug/net8.0/CoffeeShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/CoffeeShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0631121f248c25b07277c6b5cbd8cdfaad905cc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f99cd0661833fa75e2cc5244fcc08ce833b5a17b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
